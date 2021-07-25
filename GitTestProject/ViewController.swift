86f8a00d958945a99fab9c6cea9bebd3e0ea6222
Ataberk Özyılmaz <ataberk279@gmail.com>
2021-07-25 18:37:41 +0300

    first commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Ataberk Özyılmaz on 25.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("git")
+    }
+
+
+}
+
